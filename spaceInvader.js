8c046c90aecc76ff8ba88465f22382edac106f6e
Opola Mote Junior <junior.opola.mote@student.ehb.be>
2022-10-12 10:47:59 +0200

    opdracht gemaakt

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,60 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawline();
+
+function drawline() {
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(50,50,100,100);
+    context.fill();
+   
+
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(50,350,100,100);
+    context.fill();
+    
+
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(150,50,100,500);
+    context.fill();
+    
+
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(250,250,100,100);
+    context.fill();
+    
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(350,50,100,500);
+    context.fill();
+    
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(450,50,100,100);
+    context.fill();
+    
+
+
+    context.beginPath();
+    context.fillStyle = "GREEN";
+    context.rect(450,350,100,100);
+    context.fill();
+    
+
+
+
+}
